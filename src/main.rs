eecdd3f826c683d3342265ae2778b24526f81068
Teng Tokoro <tokoro10g@tokor.org>
2022-01-17 00:37:00 +0900

    Support STM32G474RE

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,27 @@
 #![no_std]
 
 use cortex_m_rt::entry;
+use hal::stm32;
+use hal::rcc::Config;
 use panic_halt as _;
-use stm32f4xx_hal as hal;
+use stm32g4xx_hal as hal;
 
-use crate::hal::{pac, prelude::*, delay::Delay};
+use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
     if let (Some(dp), Some(cp)) = (
-        pac::Peripherals::take(),
+        stm32::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        let gpioa = dp.GPIOA.split();
+        let mut rcc = dp.RCC.freeze(Config::hsi());
+        let mut delay = cp.SYST.delay(&rcc.clocks);
+
+        let gpioa = dp.GPIOA.split(&mut rcc);
         let mut led = gpioa.pa5.into_push_pull_output();
 
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
-        let mut delay = Delay::new(cp.SYST, &clocks);
-
         loop {
-            led.set_high();
-            delay.delay_ms(500_u32);
-            led.set_low();
+            led.toggle().unwrap();
             delay.delay_ms(500_u32);
         }
     }
